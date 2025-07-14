0bf4266fc4e1e40c2653e30476824693cf83bcbc
wannnii <lolme12.p@gmail.com>
2025-07-14 20:04:17 +0700

    comment

--- dlyoutube.py
+++ dlyoutube.py
@@ -1,10 +1,10 @@
 # pip install yt_dlp ติตตั้ง library yt_dlp
 
-import yt_dlp
+import yt_dlp #สวัสดี
 def download_youtube_video(url, save_path="."):
     ydl_opts = {
         'outtmpl': f'{save_path}/%(title)s.%(ext)s',
-        'format': 'best'
+        'format': 'best' 
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
