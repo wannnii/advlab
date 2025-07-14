82fabd0d11b181c5758a2ec125def0bb22febfaa
wannnii <lolme12.p@gmail.com>
2025-07-14 20:01:16 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1>Download Youtube from url</h1>
+<p>url : https://www.youtube.com/watch?v=aBJmmdGkSvk</p>
+<h2>Step 01 : Install module </h2>
+<code>pip install yt_dlp</code>
+<h2>Step 02 : Run</h2>
+<code>python app.py</code>
---
+++ dlyoutube.py
@@ -0,0 +1,12 @@
+# pip install yt_dlp ติตตั้ง library yt_dlp
+
+import yt_dlp
+def download_youtube_video(url, save_path="."):
+    ydl_opts = {
+        'outtmpl': f'{save_path}/%(title)s.%(ext)s',
+        'format': 'best'
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+video_url = input("Enter the YouTube video URL: ")
+download_youtube_video(video_url, save_path=".")
